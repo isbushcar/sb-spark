5131cc34f748aa387676214dc361d157e20c8b28
Andrey Berezin <isbushcar@gmail.com>
2023-02-19 18:32:49 +0300

    Final version

--- lab06/features/src/main/scala/features.scala
+++ lab06/features/src/main/scala/features.scala
@@ -1,6 +1,8 @@
 import org.apache.spark.ml.feature.CountVectorizerModel
+import org.apache.spark.ml.linalg.SparseVector
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.expressions.UserDefinedFunction
 import org.apache.spark.sql.functions._
-import org.apache.spark.sql.{Column, SparkSession}
 import org.apache.spark.{SparkContext, sql}
 
 
@@ -13,89 +15,113 @@ object features {
   val sc: SparkContext = spark.sparkContext
 
   def main(args: Array[String]): Unit = {
-    val logsJson: sql.DataFrame = readLog("json")
-    val logsParquet: sql.DataFrame = readLog("parquet")
+    val logsJson: sql.DataFrame = readLog()
+    logsJson.createOrReplaceTempView("logsJson")
 
-    val unionLog: sql.DataFrame = logsJson.union(logsParquet)
-    unionLog.createOrReplaceTempView("unionLog")
-
-    val top1000: sql.DataFrame = unionLog
+    val top1000: Array[String] = logsJson
       .groupBy("domain")
       .count()
       .filter("domain is not null")
       .orderBy(col("count").desc)
       .limit(1000)
       .orderBy("domain")
+      .select("domain")
+      .collectAsList()
+      .toArray
+      .map(x => x.toString.stripPrefix("[").stripSuffix("]"))
 
-    top1000.createOrReplaceTempView("top1000")
-
-    val filteredDf:sql.DataFrame = spark.sql(
-      "SELECT * FROM unionLog WHERE domain IN (SELECT domain FROM top1000)"
+    val preparedForCvm: sql.DataFrame = spark.sql(
+      "SELECT uid, collect_list(domain) as domains FROM logsJson GROUP BY uid"
     )
 
-    val dfWithTsMarks:sql.DataFrame = filteredDf
+    val cvm: sql.DataFrame = new CountVectorizerModel(top1000)
+      .setInputCol("domains")
+      .setOutputCol("domain_features")
+      .transform(preparedForCvm)
+      .withColumn("domain_features", extractArray(col("domain_features")))
+    cvm.createOrReplaceTempView("cvm")
+
+    val parsedDaysAndHours: sql.DataFrame = logsJson
       .withColumn("day", date_format(from_unixtime(col("visit.timestamp") / 1000), "E"))
       .withColumn("hour", hour(from_unixtime(col("visit.timestamp") / 1000)))
-      .withColumn("fraction_work_hours", col("hour").between(9, 17))
-      .withColumn("fraction_evening_hours", col("hour").between(18, 23))
-      .drop("visit")
+    parsedDaysAndHours.createOrReplaceTempView("parsedDaysAndHours")
+
+    val dfWithTsMarks: sql.DataFrame = spark.sql(
+      """
+       SELECT
+           log.uid,
+           log.day,
+           log.hour,
+           work.cnt / count(log.hour) OVER (PARTITION BY log.uid) AS web_fraction_work_hours,
+           evening.cnt / count(log.hour) OVER (PARTITION BY log.uid) AS web_fraction_evening_hours
+       FROM parsedDaysAndHours log
+       LEFT JOIN
+       (SELECT uid, count(hour) AS cnt
+           FROM parsedDaysAndHours
+           WHERE hour between 9 and 17
+           GROUP BY uid) AS work
+       USING (uid)
+       LEFT JOIN
+       (SELECT uid, count(hour) AS cnt
+           FROM parsedDaysAndHours
+           WHERE hour between 18 and 23
+           GROUP BY uid) AS evening
+       USING (uid)
+       """
+    )
 
     dfWithTsMarks.createOrReplaceTempView("dfWithTsMarks")
 
-    val pivotDays: sql.DataFrame = spark.sql(
-      "SELECT " +
-          "uid, " +
-          "domain, " +
-          "'web_day_' || lower(day) as day, " +
-          "'web_hour_' || CAST(hour AS String) as hour, " +
-          "CAST(fraction_work_hours AS Int) as fraction_work_hours, " +
-          "CAST(fraction_evening_hours AS Int) as fraction_evening_hours " +
-        "FROM dfWithTsMarks"
-    ).groupBy("uid", "domain", "fraction_work_hours", "fraction_evening_hours", "hour")
+    val preparedDf: sql.DataFrame = spark.sql(
+      """
+         SELECT
+          uid,
+          'web_day_' || lower(day) as day,
+          'web_hour_' || CAST(hour AS String) as hour,
+          web_fraction_work_hours,
+          web_fraction_evening_hours
+        FROM dfWithTsMarks
+        """
+    )
+
+    val pivotDays: sql.DataFrame = preparedDf
+      .drop("hour")
+      .groupBy("uid", "web_fraction_work_hours", "web_fraction_evening_hours")
       .pivot("day")
       .count()
       .na.fill(0)
+    pivotDays.createOrReplaceTempView("pivotDays")
 
-    val pivotDf: sql.DataFrame = pivotDays
-      .groupBy("uid", pivotDays.columns.filter(x => x != "uid" && x != "hour"): _*)
+    val pivotHours: sql.DataFrame = preparedDf
+      .drop("day")
+      .groupBy("uid", "web_fraction_work_hours", "web_fraction_evening_hours")
       .pivot("hour")
       .count()
       .na.fill(0)
+    pivotHours.createOrReplaceTempView("pivotHours")
+
+    val pivotDf = spark.sql("""
+        SELECT
+          *
+         FROM pivotDays
+         FULL OUTER JOIN pivotHours
+         USING (uid, web_fraction_work_hours, web_fraction_evening_hours)
+      """)
 
     pivotDf.createOrReplaceTempView("pivotDf")
 
-    val aggCols: String = pivotDf
-      .columns
-      .filter(x => x != "uid" && x != "domain")
-      .map(x => s"sum($x) as $x")
-      .mkString(", ")
-
-    val aggDf: sql.DataFrame = spark.sql(
-      "SELECT " +
-        "uid, " +
-        "collect_list(domain) as domains, " +
-        aggCols +
-      " FROM pivotDf GROUP BY uid"
-    )
-
-    val topArr: Array[String] = top1000.select("domain").collectAsList().toArray.map(x => x.toString)
-
-    val cvmDf: sql.DataFrame = new CountVectorizerModel(topArr)
-      .setInputCol("domains")
-      .setOutputCol("domains_features")
-      .transform(aggDf)
-
     val previousMatrix: sql.DataFrame = readPreviousMatrix()
+    previousMatrix.createOrReplaceTempView("prev")
 
-    val currentCols: Set[String] = cvmDf.columns.toSet
-    val previousCols: Set[String] = previousMatrix.columns.toSet
-    val bothDfCols: Set[String] = currentCols ++ previousCols
-
-    val unitedDf: sql.DataFrame = cvmDf
-      .select(addMissingCols(currentCols, bothDfCols): _*)
-      .unionByName(
-        previousMatrix.select(addMissingCols(previousCols, bothDfCols): _*)
-      )
+    val unitedDf: sql.DataFrame = spark.sql("""
+        SELECT
+          *
+        FROM pivotDf
+        LEFT JOIN cvm
+        USING (uid)
+        FULL OUTER JOIN prev
+        USING (uid)
+        """)
 
     unitedDf
       .write
@@ -103,11 +129,11 @@ object features {
       .save("/user/andrey.berezin/features")
   }
 
-  private def readLog(logFormat: String): sql.DataFrame = {
+  private def readLog(): sql.DataFrame = {
     spark
       .read
-      .format(logFormat)
-      .load(s"/labs/laba03/weblogs.$logFormat")
+      .format("json")
+      .load(s"/labs/laba03/weblogs.json")
       .select(col("uid"), explode(col("visits")).as("visit"))
       .withColumn("host", lower(callUDF("parse_url", col("visit.url"), lit("HOST"))))
       .withColumn("domain", regexp_replace(col("host"), "www.", ""))
@@ -121,10 +147,9 @@ object features {
       .load("/user/andrey.berezin/users-items/20200429")
   }
 
-  private def addMissingCols(existing: Set[String], allNeeded: Set[String]): List[Column] = {
-    allNeeded.toList.map {
-      case x if existing.contains(x) => col(x)
-      case x => lit(null).as(x)
+  private def extractArray: UserDefinedFunction = udf(
+    (feature: Any) => {
+      feature.asInstanceOf[SparseVector].toArray.map(x => x.toInt)
     }
-  }
+  )
 }
